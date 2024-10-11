39c50f4318743597f312651a4c6ed60b2d85650c
Anggoro Yudho N. <70189451+anggoro-yn@users.noreply.github.com>
2024-10-11 13:12:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -188,7 +188,7 @@ st.write(kelulusan_df)
 colors = ['#4682B4'] * len(kelulusan_df)  # Warna Steel Blue
 top_three_indices = kelulusan_df.head(3).index.tolist()  # Indeks tiga teratas
 for index in top_three_indices:
-    colors[index] = '#1E90FF'  # Warna Dodger Blue untuk tiga teratas
+    colors[index] = '#4682B4'  # Warna Dodger Blue untuk tiga teratas
 
 # Membuat grafik batang menggunakan Matplotlib
 plt.figure(figsize=(10, 6))
